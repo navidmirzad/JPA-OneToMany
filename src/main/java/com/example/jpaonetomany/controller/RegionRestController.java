484673caa12ad64f4ba4b6b3a79866532ff4ca02
navidmirzad <navi0221@stud.kea.dk>
2023-09-03 19:33:59 +0200

    // DeleteMapping("/region/{kode}") works

--- src/main/java/com/example/jpaonetomany/controller/RegionRestController.java
+++ src/main/java/com/example/jpaonetomany/controller/RegionRestController.java
@@ -1,18 +1,27 @@
 package com.example.jpaonetomany.controller;
 
 import com.example.jpaonetomany.Model.Region;
+import com.example.jpaonetomany.repositories.RegionRepository;
 import com.example.jpaonetomany.service.ApiServiceGetRegioner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class RegionRestController {
 
     @Autowired
     ApiServiceGetRegioner apiServiceGetRegioner;
+
+    @Autowired
+    RegionRepository regionRepository;
 
     @GetMapping("/getregioner")
     public List<Region> getRegioner() {
@@ -24,7 +33,15 @@ public class RegionRestController {
 
     // add PutMapping for Regioner
 
-    // add DeleteMapping for Regioner
+    @DeleteMapping("/region/{kode}")
+    public ResponseEntity<String> deleteRegion(@PathVariable String kode) {
+        try {
+            regionRepository.deleteById(kode);
+            return ResponseEntity.ok().build();
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Region not found");
+        }
+    }
 
 
 }
--- src/main/java/com/example/jpaonetomany/repositories/RegionRepository.java
+++ src/main/java/com/example/jpaonetomany/repositories/RegionRepository.java
@@ -3,7 +3,8 @@ package com.example.jpaonetomany.repositories;
 import com.example.jpaonetomany.Model.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RegionRepository extends JpaRepository<Region, String> {
+import java.util.List;
 
+public interface RegionRepository extends JpaRepository<Region, String> {
 
 }
